e367d0707b8f1d5f88bddf7c84351e0bb2c784db
Geoffrey Gane <geoffreygane@gmail.com>
2023-11-05 04:30:54 +0100

    ref: inline content update in byte count use case

--- lib/use_cases/count_bytes.ex
+++ lib/use_cases/count_bytes.ex
@@ -4,10 +4,6 @@ defmodule UseCase.CountBytes do
   alias Entities.Content
 
   def count(content) do
-    %Content{
-      file_name: content.file_name,
-      binary: content.binary,
-      bytes: byte_size(content.binary)
-    }
+    %Content{content | bytes: byte_size(content.binary)}
   end
 end
